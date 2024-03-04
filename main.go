2adc2d5b44ee44f47a80a534ea4c0f13991d37d1
atanda0x <atandanafiu21@gmail.com>
2024-03-04 08:16:19 -0800

    added handlers

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
 	file := http.FileServer(http.Dir("/public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", file))
 
-	// mux.HandleFunc("/", index)
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/err", err)
+
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signp_account", signp_account)
+	mux.HandleFunc("/auth", auth)
+
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -15,4 +30,16 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html"}
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads()
+	if err == nil {
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
 }
